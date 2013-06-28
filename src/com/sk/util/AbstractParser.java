bc4ecd3eff5dd3a722e723e23978b4a92ddaa000
Strikeskids <github@strikeskids.com>
2013-06-28 11:24:23 -0400

    Reorganized code. Made parser interface. Added AbstractParser implementing loads

---
+++ src/com/sk/util/AbstractParser.java
@@ -0,0 +1,35 @@
+package com.sk.util;
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.jsoup.Jsoup;
+import org.jsoup.helper.HttpConnection;
+import org.jsoup.nodes.Document;
+
+/**
+ * Implements the load methods of the basic parser class. Classes should extend this and use the {@link #doc}
+ * field to grab data
+ * 
+ * @author Strikeskids
+ * 
+ */
+public abstract class AbstractParser implements Parser {
+
+	protected Document doc;
+
+	public AbstractParser() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void load(URL url) throws IOException {
+		doc = HttpConnection.connect(url).get();
+	}
+
+	@Override
+	public void load(String source, String baseURI) {
+		doc = Jsoup.parse(source, baseURI);
+	}
+
+}
--- src/com/sk/util/Scraper.java
+++ src/com/sk/util/Scraper.java
@@ -1,36 +1,12 @@
 package com.sk.util;
 
-import java.io.IOException;
-import java.net.URL;
-
-public interface Scraper {
-	/**
-	 * Attempts to load HTML from the source {@link URL}
-	 * 
-	 * @param url
-	 *            The source {@link URL} to get
-	 * @throws IOException
-	 *             if the file failed to be loaded
-	 */
-	public void load(URL url) throws IOException;
-
-	/**
-	 * Attempts to load HTML from the source {@link String}
-	 * 
-	 * @param source
-	 *            A {@link String} containing the HTML
-	 */
-	public void load(String source);
-
-	/**
-	 * Attempts to parse the loaded HTML. Use this method after loading the data
-	 * 
-	 * @return <code>true</code> if the scraper parsed the HTML successfully and extracted data;
-	 *         <code>false</code> otherwise
-	 * @throws IllegalStateException
-	 *             if attempting to parse before loading the data
-	 */
-	public boolean parse() throws IllegalStateException;
+/**
+ * A {@link PersonalData} scraper. Scrapes from various website sources
+ * 
+ * @author Strikeskids
+ * 
+ */
+public interface Scraper extends Parser {
 
 	/**
 	 * Gets the {@link PersonalData} that was parsed from the loaded HTML. Use this method after
