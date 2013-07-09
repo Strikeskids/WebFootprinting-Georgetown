114fa6f6b69a7d7aa27277043ec41e4b95fd27f8
Strikeskids <github@strikeskids.com>
2013-07-09 12:31:57 -0400

    Added get id to scraper

--- src/com/sk/impl/GooglePlusScraper.java
+++ src/com/sk/impl/GooglePlusScraper.java
@@ -1,9 +1,12 @@
 package com.sk.impl;
 
+import java.util.regex.Pattern;
+
 import com.sk.util.SiteScraperInfo;
 import com.sk.util.parse.scrape.BasicGrabber;
 import com.sk.util.parse.scrape.Grabber;
 import com.sk.util.parse.scrape.GrabberSiteScraper;
+import com.sk.util.parse.scrape.RegexGrabber;
 
 @SiteScraperInfo(siteBase = "https://plus.google.com", siteId = "g+")
 public class GooglePlusScraper extends GrabberSiteScraper {
@@ -11,7 +14,8 @@ public class GooglePlusScraper extends GrabberSiteScraper {
 	private static final Grabber[] grabbers = { new BasicGrabber("div[guidedhelpid=profile_name]", "name"),
 			new BasicGrabber("span:contains(Works) + span.FG2oob", "job-title"),
 			new BasicGrabber("span:contains(Attended) + span.FG2oob", "education"),
-			new BasicGrabber("span:contains(Lives) + span.FG2oob", "location") };
+			new BasicGrabber("span:contains(Lives) + span.FG2oob", "location"),
+			new RegexGrabber(".g-x-Aa.ud-Aa", "token", Pattern.compile("(?<name>[0-9]+)/"), "uid") };
 
 	public GooglePlusScraper() {
 		super(grabbers);
