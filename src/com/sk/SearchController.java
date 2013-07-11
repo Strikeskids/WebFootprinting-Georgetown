e989815a74ed8cd8737e47f73f390d1a57b3396b
Strikeskids <github@strikeskids.com>
2013-07-11 15:59:22 -0400

    Made Searcher use ThreadPool and cut time spent in quarter

--- src/com/sk/SearchController.java
+++ src/com/sk/SearchController.java
@@ -2,12 +2,21 @@ package com.sk;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import com.sk.api.impl.FourSquareApiSearcher;
 import com.sk.api.impl.GooglePlusApiSearcher;
 import com.sk.api.impl.LinkedinApiSearcher;
 import com.sk.api.impl.PiplApiSearcher;
-import com.sk.impl.search.AllNameSearcher;
+import com.sk.impl.search.GooglePlusSearcher;
+import com.sk.impl.search.LinkedInSearcher;
+import com.sk.impl.search.WhitepagesSearcher;
 import com.sk.util.PersonalData;
 import com.sk.util.PersonalDataStorage;
 import com.sk.util.parse.search.NameSearcher;
@@ -30,8 +39,14 @@ import com.sk.util.parse.search.NameSearcher;
 public class SearchController implements NameSearcher {
 
 	private final ThreadLocal<PersonalDataStorage> store = new ThreadLocal<>();
-	private NameSearcher[] use = { new AllNameSearcher(), new LinkedinApiSearcher(), new PiplApiSearcher(),
-			new FourSquareApiSearcher(), new GooglePlusApiSearcher() };
+	private NameSearcher[] use = { new GooglePlusSearcher(), new LinkedInSearcher(), new WhitepagesSearcher(),
+			new LinkedinApiSearcher(), new PiplApiSearcher(), new FourSquareApiSearcher(),
+			new GooglePlusApiSearcher() };
+	private final ExecutorService executor;
+
+	public SearchController() {
+		executor = Executors.newCachedThreadPool();
+	}
 
 	@Override
 	public URL[] results() throws IllegalStateException {
@@ -56,15 +71,48 @@ public class SearchController implements NameSearcher {
 		boolean ret = false;
 		this.store.remove();
 		PersonalDataStorage store = new PersonalDataStorage();
+		List<Future<PersonalData[]>> futures = new ArrayList<>();
 		for (NameSearcher n : use) {
-			boolean success = n.lookForName(first, last);
-			ret |= success;
-			if (success)
-				store.add(n.getData());
+			futures.add(executor.submit(new SearchRunnable(n, first, last)));
+		}
+		for (int i = 0; i < futures.size(); ++i) {
+			try {
+				Future<PersonalData[]> fut = futures.get(i);
+				PersonalData[] dat = fut.get();
+				if (dat.length > 0) {
+					ret = true;
+					store.add(dat);
+				}
+			} catch (InterruptedException e) {
+				--i;
+				continue;
+			} catch (ExecutionException e) {
+				continue;
+			}
 		}
 		if (ret)
 			this.store.set(store);
 		return ret;
+	}
+
+	private class SearchRunnable implements Callable<PersonalData[]> {
+
+		private final NameSearcher ns;
+		private final String first, last;
+
+		private SearchRunnable(NameSearcher ns, String first, String last) {
+			this.ns = ns;
+			this.first = first;
+			this.last = last;
+		}
+
+		@Override
+		public PersonalData[] call() throws Exception {
+			if (ns.lookForName(first, last))
+				return ns.getData();
+			else
+				return new PersonalData[0];
+		}
 	}
 
 }
