245ea8508f0f3289ee698c9b0620bd0346eb1103
Strikeskids <github@strikeskids.com>
2013-07-15 10:44:07 -0400

    Used reflection to improve PiplApiSearcher

--- src/com/sk/api/impl/PiplApiSearcher.java
+++ src/com/sk/api/impl/PiplApiSearcher.java
@@ -1,6 +1,8 @@
 package com.sk.api.impl;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.text.DateFormat;
@@ -53,55 +55,29 @@ public class PiplApiSearcher implements NameSearcher {
 		List<URL> url = new ArrayList<>();
 		for (Record possible : resp.getRecords()) {
 			PersonalData cur = new PersonalData("pipl");
-			if (possible.getNames() != null)
-				for (Name n : possible.getNames()) {
-					cur.put("name", n.getRaw());
-					cur.put("first-name", n.getFirst());
-					cur.put("last-name", n.getLast());
-					break;
-				}
-			if (possible.getAddresses() != null)
-				for (Address a : possible.getAddresses()) {
-					cur.put("address", a.getRaw());
-					cur.put("country", a.getCountry());
-					cur.put("city", a.getCity());
-					cur.put("house", a.getHouse());
-					cur.put("apartament", a.getApartment());
-					cur.put("po_box", a.getPoBox());
-					cur.put("street", a.getStreet());
-					cur.put("state", a.getState());
-					break;
-				}
-			if (possible.getPhones() != null)
+
+			if (possible.getPhones() != null) {
 				for (Phone ph : possible.getPhones()) {
 					cur.put(ph.getType().replaceAll("_", "-"), ph.getDisplay());
 				}
-			if (possible.getEmails() != null)
-				for (Email e : possible.getEmails()) {
-					cur.put("email", e.getAddress());
-					break;
-				}
-			if (possible.getUsernames() != null)
-				for (Username u : possible.getUsernames()) {
-					cur.put("username", u.getContent());
-					break;
-				}
-			if (possible.getDobs() != null)
+			}
+			if (possible.getDobs() != null) {
 				for (DOB d : possible.getDobs()) {
 					cur.put("age", d.age() + "");
 					cur.put("dob", DateFormat.getDateInstance().format(d.getDateRange().middle()));
 					break;
 				}
-			if (possible.getJobs() != null)
-				for (Job j : possible.getJobs()) {
-					cur.put("job-title", j.getTitle());
-					cur.put("industry", j.getIndustry());
-					break;
-				}
-			if (possible.getEducations() != null)
-				for (Education e : possible.getEducations()) {
-					cur.put("education", e.display());
-				}
+			}
+			grab(possible, cur, Email.class, new String[] { "Address" }, new String[] { "email" });
+			grab(possible, cur, Username.class, new String[] { "Content" }, new String[] { "username" });
+			grab(possible, cur, Job.class, new String[] { "Title", "Industry" }, new String[] { "job-title",
+					"industry" });
+			grab(possible, cur, Education.class, new String[] { "display" }, new String[] { "education" });
+			grab(possible, cur, Name.class, new String[] { "display", "First", "Last" }, new String[] { "name",
+					"first-name", "last-name" });
+			grab(possible, cur, Address.class, new String[] { "Raw", "Country", "City", "House", "PoBox",
+					"Apartment", "Street", "State" }, new String[] { "address", "country", "city", "house",
+					"po-box", "apartment", "street", "state" });
 			if (scrape.isValid(possible.getSource().getUrl()))
 				url.add(new URL(possible.getSource().getUrl()));
 			data.add(cur);
@@ -109,6 +85,55 @@ public class PiplApiSearcher implements NameSearcher {
 		urls.set(url.toArray(new URL[url.size()]));
 		this.data.set(data.toArray(new PersonalData[data.size()]));
 		return true;
+	}
+
+	private void grab(Record record, PersonalData store, Class<?> clazz, String[] methodNames,
+			String[] attributeNames) {
+		if (methodNames.length != attributeNames.length)
+			throw new IllegalArgumentException();
+		Class<?> recordClazz = Record.class;
+
+		Method source = null;
+		for (Method m : recordClazz.getMethods()) {
+			if (m.getName().toUpperCase().contains(clazz.getSimpleName().toUpperCase())) {
+				source = m;
+				break;
+			}
+		}
+		if (source == null)
+			throw new IllegalArgumentException();
+		Method[] grabbers = new Method[methodNames.length];
+		StringBuilder[] builders = new StringBuilder[methodNames.length];
+		try {
+			for (int i = 0; i < methodNames.length; ++i) {
+				grabbers[i] = clazz.getDeclaredMethod(Character.isUpperCase(methodNames[i].charAt(0)) ? "get"
+						+ methodNames[i] : methodNames[i]);
+				builders[i] = new StringBuilder();
+			}
+		} catch (NoSuchMethodException ex) {
+			throw new IllegalArgumentException();
+		}
+		try {
+			Object list = source.invoke(record);
+			if (list == null)
+				return;
+
+			for (Object item : (ArrayList<?>) list) {
+				for (int j = 0; j < methodNames.length; ++j) {
+					Object grabbed = grabbers[j].invoke(item);
+					if (grabbed != null)
+						builders[j].append(grabbed);
+					builders[j].append("|");
+				}
+			}
+		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			throw new IllegalArgumentException();
+		}
+		for (int i = 0; i < methodNames.length; ++i) {
+			if (builders[i].length() > 1) {
+				store.put(attributeNames[i], builders[i].substring(0, builders[i].length() - 1));
+			}
+		}
 	}
 
 	@Override
