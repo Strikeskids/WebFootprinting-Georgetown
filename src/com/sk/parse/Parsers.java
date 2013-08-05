e7dc5ab71591704eec270e463da4ec044bbf9698
Strikeskids <github@strikeskids.com>
2013-08-05 12:34:45 -0400

    Added parsers

---
+++ src/com/sk/parse/Parsers.java
@@ -0,0 +1,25 @@
+package com.sk.parse;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.parser.Parser;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+public class Parsers {
+
+	private static final JsonParser jsonParser = new JsonParser();
+
+	public static JsonElement parseJSON(String input) {
+		return jsonParser.parse(input);
+	}
+
+	public static Document parseXML(String input) {
+		return Jsoup.parse(input, "", Parser.xmlParser());
+	}
+
+	public static Document parseHTML(String input, String baseUri) {
+		return Jsoup.parse(input, baseUri, Parser.htmlParser());
+	}
+}
