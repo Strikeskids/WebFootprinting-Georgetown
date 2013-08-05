9d7e1680000377d49ba1bc59426172683bb44298
Strikeskids <github@strikeskids.com>
2013-08-05 13:04:31 -0400

    Added OuterLoader

---
+++ src/com/sk/parse/OuterLoader.java
@@ -0,0 +1,40 @@
+package com.sk.parse;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import com.sk.Driver;
+import com.sk.util.LazyField;
+import com.sk.util.PersonalData;
+
+public abstract class OuterLoader extends PagingLoader {
+
+	private LazyField<List<PersonalData>> ownResults = new LazyField<>(new Callable<List<PersonalData>>() {
+		@Override
+		public List<PersonalData> call() throws Exception {
+			return loadOwnResults();
+		}
+	});
+
+	@Override
+	public List<PersonalData> getOwnResults() {
+		return ownResults.get();
+	}
+
+	private List<PersonalData> loadOwnResults() {
+		List<PersonalData> ret = new ArrayList<>();
+		try {
+			List<Future<List<PersonalData>>> futures = Driver.EXECUTOR.invokeAll(getExtractors());
+			for (Future<List<PersonalData>> taskResult : futures) {
+				ret.addAll(taskResult.get());
+			}
+		} catch (InterruptedException | ExecutionException ignored) {
+		}
+		return ret;
+	}
+
+	protected abstract List<Extractor> getExtractors();
+}
