9e24f92466a9110b137630fe31c5d9a832c5a3b7
Strikeskids <github@strikeskids.com>
2013-08-05 14:37:16 -0400

    Added default StringProcessor

--- src/com/sk/util/StringProcessor.java
+++ src/com/sk/util/StringProcessor.java
@@ -2,4 +2,11 @@ package com.sk.util;
 
 public interface StringProcessor {
 	public String process(String input);
+
+	public static final StringProcessor NIL = new StringProcessor() {
+		@Override
+		public String process(String input) {
+			return input;
+		}
+	};
 }
