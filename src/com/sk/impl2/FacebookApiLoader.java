f3d40daa2afe1532fed94fcc1f514959eab783af
Strikeskids <github@strikeskids.com>
2013-08-05 16:36:37 -0400

    Added Facebook

---
+++ src/com/sk/impl2/FacebookApiLoader.java
@@ -0,0 +1,52 @@
+package com.sk.impl2;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.sk.api.NameComparison;
+import com.sk.parse.Extractor;
+
+public class FacebookApiLoader extends GoogleSearchLoader {
+
+	private static final Pattern URL_PATTERN = Pattern.compile("^https?://www\\.facebook\\.com/([^/]+)$");
+	private static final Pattern TITLE_PATTERN = Pattern.compile("(.*?) [|] Facebook");
+
+	static final String SITE_KEY = "facebook";
+
+	public FacebookApiLoader(final String first, final String last) {
+		super("site:facebook.com " + first + " " + last, new SearchAcceptor() {
+			private final String[] names = new String[] { first, last };
+
+			@Override
+			public Extractor getExtractor(SearchResult result) {
+				String id = getId(result);
+				if (id == null)
+					return null;
+				String[] currentNames = getNames(result);
+				if (currentNames == null || !NameComparison.get().isSameName(names, currentNames))
+					return null;
+				return new FacebookPersonLoader(id);
+			}
+		});
+	}
+
+	private static String getId(SearchResult result) {
+		Matcher urlMatcher = URL_PATTERN.matcher(result.url);
+		if (urlMatcher.find())
+			return urlMatcher.group(1);
+		else
+			return null;
+	}
+
+	private static String[] getNames(SearchResult result) {
+		Matcher titleMatcher = TITLE_PATTERN.matcher(result.title);
+		if (titleMatcher.find())
+			return parseName(titleMatcher.group(1));
+		return null;
+	}
+
+	private static String[] parseName(String group) {
+		return NameComparison.get().parseName(group);
+	}
+
+}
---
+++ src/com/sk/impl2/FacebookPersonLoader.java
@@ -0,0 +1,71 @@
+package com.sk.impl2;
+
+import static com.sk.impl2.FacebookApiLoader.SITE_KEY;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+
+import com.google.gson.JsonObject;
+import com.sk.parse.AbstractLoader;
+import com.sk.parse.Extractor;
+import com.sk.parse.Parsers;
+import com.sk.util.DocNavigator;
+import com.sk.util.FieldBuilder;
+import com.sk.util.PersonalData;
+import com.sk.web.IOUtil;
+import com.sk.web.OAuthRequest;
+import com.sk.web.Request;
+
+public class FacebookPersonLoader extends AbstractLoader implements Extractor {
+
+	private static final String BASE_URL = "https://graph.facebook.com/%s";
+
+	private final String url;
+
+	private JsonObject json;
+
+	public FacebookPersonLoader(String id) {
+		this.url = String.format(BASE_URL, IOUtil.urlEncode(id));
+	}
+
+	@Override
+	protected Request getRequest() {
+		try {
+			OAuthRequest request = new OAuthRequest(url, "GET");
+			request.signOAuth(ApiUtility.getConsumerToken(SITE_KEY), ApiUtility.getOAuthToken(SITE_KEY));
+			request.addRandomUserAgent();
+			return request;
+		} catch (MalformedURLException ignored) {
+			return null;
+		}
+	}
+
+	@Override
+	protected void parse(URL source, String data) {
+		json = Parsers.parseJSON(data).getAsJsonObject();
+	}
+
+	@Override
+	public List<PersonalData> call() throws Exception {
+		return getResults();
+	}
+
+	@Override
+	public List<PersonalData> getResults() {
+		FieldBuilder builder = new FieldBuilder();
+		PersonalData ret = new PersonalData(SITE_KEY);
+		for (DocNavigator navigator : navigators) {
+			navigator.navigate(json, builder);
+		}
+		builder.joinNames();
+		builder.addTo(ret);
+		return Arrays.asList(ret);
+	}
+
+	private static final DocNavigator[] navigators = { new DocNavigator("firstName", "first_name"),
+			new DocNavigator("lastName", "last_name"), new DocNavigator("gender", "gender"),
+			new DocNavigator("email", "email"), new DocNavigator("id", "id") };
+
+}
