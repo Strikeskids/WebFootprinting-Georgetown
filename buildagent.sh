a56806d0d5c32a6ed3a24db48d985cee26f6c785
Strikeskids <github@strikeskids.com>
2013-08-08 09:30:25 -0400

    Added network interface cycling

--- buildagent.sh
+++ buildagent.sh
@@ -2,6 +2,6 @@
 cd bin
 
 echo "Agent-Class: BindIPAgent\nManifest-Version: 1.0" > tmp
-jar -cvmf tmp ../agent.jar BindIPAgent*.class NetworkAddresses.class
+jar -cvmf tmp ../agent.jar BindIPAgent*.class NetworkAddresses*.class
 rm tmp
-jar -cvf ../boot.jar NetworkAddresses.class
+jar -cvf ../boot.jar NetworkAddresses*.class
--- src/NetworkAddresses.java
+++ src/NetworkAddresses.java
@@ -1,8 +1,70 @@
-
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.NetworkInterface;
 import java.net.Socket;
+import java.net.SocketException;
+import java.util.Collections;
+import java.util.Enumeration;
 
 public class NetworkAddresses {
+
+	private static InetAddressNode addresses;
+	private static final Object addressLock = new Object();
+
 	public static void modifySocket(Socket s) {
-		System.out.println("MODIFY SOCKET");
+		InetAddress add = nextAddress();
+		try {
+			s.bind(new InetSocketAddress(add, 0));
+		} catch (IOException e) {
+			System.out.println("Failed to bind socket");
+		}
+	}
+
+	private static InetAddress nextAddress() {
+		loadAddresses();
+		synchronized (addressLock) {
+			InetAddress ret = addresses.current;
+			addresses = addresses.next;
+			return ret;
+		}
+	}
+
+	private static void loadAddresses() {
+		if (addresses != null)
+			return;
+		synchronized (addressLock) {
+			try {
+				for (NetworkInterface net : Collections.list(NetworkInterface.getNetworkInterfaces())) {
+					if (!net.getDisplayName().contains("lo")) {
+						addresses = new InetAddressNode(net.getInetAddresses());
+						break;
+					}
+				}
+			} catch (SocketException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private static class InetAddressNode {
+		private InetAddress current;
+		private InetAddressNode next;
+
+		public InetAddressNode(Enumeration<InetAddress> addresses) {
+			if (!addresses.hasMoreElements())
+				throw new IllegalArgumentException();
+			current = addresses.nextElement();
+			if (!addresses.hasMoreElements()) {
+				next = this;
+			} else {
+				next = new InetAddressNode(addresses);
+				InetAddressNode tmp = next;
+				while (tmp.next != tmp) {
+					tmp = tmp.next;
+				}
+				tmp.next = this;
+			}
+		}
 	}
 }
